3258746f6e4181aa253f7de67f0bcc529e371c7f
Alexandre Lamberty <alexandrelamberty@gmail.com>
2024-03-31 17:22:34 +0200

    feat: lib generate pdf labels

--- pittacium.go
+++ pittacium.go
@@ -1,4 +1,4 @@
-package labels
+package pittacium
 
 import (
 	"fmt"
@@ -51,103 +51,32 @@ var (
 	Label3x2     = LabelFormat{Name: "3x2", WidthMM: 76.2, HeightMM: 50.8}
 )
 
-func main() {
+func GenerateLabels(products []Product, document Document) *fpdf.Fpdf {
 	// Create image directory if it doesn't exist
 	if _, err := os.Stat(imagePath); os.IsNotExist(err) {
 		os.Mkdir(imagePath, 0755)
-	}
-
-	// A list of products
-	products := []Product{
-		{Code: "5400141850574", Name: "Cereal Flakes", Price: 1.72},
-		{Code: "5410091734367", Name: "Déo Divine Moments (Fa)", Price: 1.66},
-		{Code: "540091719838", Name: "Déo Glamorous Moments (Fa)", Price: 1.66},
-		{Code: "5410091729745", Name: "Déo Pink Passion (Fa)", Price: 2.99},
-		{Code: "5400141437102", Name: "Eponges x6 (Boni)", Price: 2.38},
-		{Code: "5400141358858", Name: "Frost Cribb (Boni)", Price: 2.98},
-		{Code: "5400141367461", Name: "Haricots blancs Bio (Boni)", Price: 1.45},
-		{Code: "5400141058383", Name: "Haricots verts entier", Price: 1.49},
-		{Code: "5400141710595", Name: "Lait de coco (Boni)", Price: 0.99},
-		{Code: "5400141850574", Name: "Cereal Flakes", Price: 1.72},
-		{Code: "5410091734367", Name: "Déo Divine Moments (Fa)", Price: 1.66},
-		{Code: "540091719838", Name: "Déo Glamorous Moments (Fa)", Price: 1.66},
-		{Code: "5410091729745", Name: "Déo Pink Passion (Fa)", Price: 2.99},
-		{Code: "5400141437102", Name: "Eponges x6 (Boni)", Price: 2.38},
-		{Code: "5400141358858", Name: "Frost Cribb (Boni)", Price: 2.98},
-		{Code: "5400141367461", Name: "Haricots blancs Bio (Boni)", Price: 1.45},
-		{Code: "5400141058383", Name: "Haricots verts entier", Price: 1.49},
-		{Code: "5400141710595", Name: "Lait de coco (Boni)", Price: 0.99},
-		{Code: "5400141850574", Name: "Cereal Flakes", Price: 1.72},
-		{Code: "5410091734367", Name: "Déo Divine Moments (Fa)", Price: 1.66},
-		{Code: "540091719838", Name: "Déo Glamorous Moments (Fa)", Price: 1.66},
-		{Code: "5410091729745", Name: "Déo Pink Passion (Fa)", Price: 2.99},
-		{Code: "5400141437102", Name: "Eponges x6 (Boni)", Price: 2.38},
-		{Code: "5400141358858", Name: "Frost Cribb (Boni)", Price: 2.98},
-		{Code: "5400141367461", Name: "Haricots blancs Bio (Boni)", Price: 1.45},
-		{Code: "5400141058383", Name: "Haricots verts entier", Price: 1.49},
-		{Code: "5400141710595", Name: "Lait de coco (Boni)", Price: 0.99},
-		{Code: "5400141850574", Name: "Cereal Flakes", Price: 1.72},
-		{Code: "5410091734367", Name: "Déo Divine Moments (Fa)", Price: 1.66},
-		{Code: "540091719838", Name: "Déo Glamorous Moments (Fa)", Price: 1.66},
-		{Code: "5410091729745", Name: "Déo Pink Passion (Fa)", Price: 2.99},
-		{Code: "5400141437102", Name: "Eponges x6 (Boni)", Price: 2.38},
-		{Code: "5400141358858", Name: "Frost Cribb (Boni)", Price: 2.98},
-		{Code: "5400141367461", Name: "Haricots blancs Bio (Boni)", Price: 1.45},
-		{Code: "5400141058383", Name: "Haricots verts entier", Price: 1.49},
-		{Code: "5400141710595", Name: "Lait de coco (Boni)", Price: 0.99},
-		{Code: "5400141850574", Name: "Cereal Flakes", Price: 1.72},
-		{Code: "5410091734367", Name: "Déo Divine Moments (Fa)", Price: 1.66},
-		{Code: "540091719838", Name: "Déo Glamorous Moments (Fa)", Price: 1.66},
-		{Code: "5410091729745", Name: "Déo Pink Passion (Fa)", Price: 2.99},
-		{Code: "5400141437102", Name: "Eponges x6 (Boni)", Price: 2.38},
-		{Code: "5400141358858", Name: "Frost Cribb (Boni)", Price: 2.98},
-		{Code: "5400141367461", Name: "Haricots blancs Bio (Boni)", Price: 1.45},
-		{Code: "5400141058383", Name: "Haricots verts entier", Price: 1.49},
-		{Code: "5400141710595", Name: "Lait de coco (Boni)", Price: 0.99},
-		{Code: "5400141850574", Name: "Cereal Flakes", Price: 1.72},
-		{Code: "5410091734367", Name: "Déo Divine Moments (Fa)", Price: 1.66},
-		{Code: "540091719838", Name: "Déo Glamorous Moments (Fa)", Price: 1.66},
-		{Code: "5410091729745", Name: "Déo Pink Passion (Fa)", Price: 2.99},
-		{Code: "5400141437102", Name: "Eponges x6 (Boni)", Price: 2.38},
-		{Code: "5400141358858", Name: "Frost Cribb (Boni)", Price: 2.98},
-		{Code: "5400141367461", Name: "Haricots blancs Bio (Boni)", Price: 1.45},
-		{Code: "5400141058383", Name: "Haricots verts entier", Price: 1.49},
-		{Code: "5400141710595", Name: "Lait de coco (Boni)", Price: 0.99},
-		{Code: "5400141850574", Name: "Cereal Flakes", Price: 1.72},
-		{Code: "5410091734367", Name: "Déo Divine Moments (Fa)", Price: 1.66},
-		{Code: "540091719838", Name: "Déo Glamorous Moments (Fa)", Price: 1.66},
-		{Code: "5410091729745", Name: "Déo Pink Passion (Fa)", Price: 2.99},
-		{Code: "5400141437102", Name: "Eponges x6 (Boni)", Price: 2.38},
-		{Code: "5400141358858", Name: "Frost Cribb (Boni)", Price: 2.98},
-		{Code: "5400141367461", Name: "Haricots blancs Bio (Boni)", Price: 1.45},
-		{Code: "5400141058383", Name: "Haricots verts entier", Price: 1.49},
-		{Code: "5400141710595", Name: "Lait de coco (Boni)", Price: 0.99},
-		{Code: "5400141850574", Name: "Cereal Flakes", Price: 1.72},
-		{Code: "5410091734367", Name: "Déo Divine Moments (Fa)", Price: 1.66},
-		{Code: "540091719838", Name: "Déo Glamorous Moments (Fa)", Price: 1.66},
-		{Code: "5410091729745", Name: "Déo Pink Passion (Fa)", Price: 2.99},
-		{Code: "5400141437102", Name: "Eponges x6 (Boni)", Price: 2.38},
-		{Code: "5400141358858", Name: "Frost Cribb (Boni)", Price: 2.98},
-		{Code: "5400141367461", Name: "Haricots blancs Bio (Boni)", Price: 1.45},
-		{Code: "5400141058383", Name: "Haricots verts entier", Price: 1.49},
-		{Code: "5400141710595", Name: "Lait de coco (Boni)", Price: 0.99},
-	}
-
-	document := Document{
-		Title:       "Products",
-		PageSize:    "A4",
-		Orientation: "P",
-		LabelFormat: Label225x125,
 	}
 
 	// Generating barcodes from products
 	generateBarcode(products)
 
 	// Generating the PDF from the products
-	generatePDF(document, products)
+	pdf := generatePDF(document, products)
 
 	// Delete the images folder
 	os.RemoveAll(imagePath)
+
+	// Output PDF to a file
+	// date := fmt.Sprintf("%d-%02d-%02d", time.Now().Year(), time.Now().Month(), time.Now().Day())
+	// err := pdf.OutputFileAndClose(date + "-" + "products-labels-" + document.LabelFormat.Name + ".pdf")
+
+	// if err != nil {
+		// log.Fatalf("Error saving PDF: %v", err)
+	// }
+
+	// fmt.Println("PDF generated successfully!")
+
+	return pdf
 }
 
 // Generate barcode image and save it in the imagePath directory
@@ -162,7 +91,7 @@ func generateBarcode(products []Product) {
 }
 
 // Generate PDF from products
-func generatePDF(document Document, products []Product) {
+func generatePDF(document Document, products []Product) *fpdf.Fpdf {
 	// Create new PDF instance
 	pdf := fpdf.New(document.Orientation, "mm", document.PageSize, "")
 	
@@ -221,16 +150,7 @@ func generatePDF(document Document, products []Product) {
 			
 		}
 	}
-
-	// Output PDF to a file
-	date := fmt.Sprintf("%d-%02d-%02d", time.Now().Year(), time.Now().Month(), time.Now().Day())
-	err := pdf.OutputFileAndClose(date + "-" + "products-labels-" + document.LabelFormat.Name + ".pdf")
-
-	if err != nil {
-		log.Fatalf("Error saving PDF: %v", err)
-	}
-
-	fmt.Println("PDF generated successfully!")
+	return pdf
 }
 
 // Function to calculate page dimensions based on PageSize and Orientation
